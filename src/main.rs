a2f3b0711879d93972d235277b599eb2199d80b3
anit <anit.rai@cyphertree.com>
2019-05-25 18:59:08 +0530

    use sh for dev env vars

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,21 @@ extern crate listenfd;
 
 use listenfd::ListenFd;
 use actix_web::{web, HttpServer, App, HttpRequest};
+use std::env;
+
 
 fn index (_req: HttpRequest) -> &'static str {
     "Hello World!"
+}
+
+fn get_address_port() -> String {
+    let address: String = env::var("ADDRESS")
+        .unwrap_or_else(|_| String::from("0.0.0.0"));
+
+    let port: String = env::var("PORT")
+        .unwrap_or_else(|_| String::from("80"));
+
+    format!("{}:{}", address, port)
 }
 
 
@@ -19,7 +31,7 @@ fn main() -> std::io::Result<()> {
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen(l).unwrap()
     } else {
-        server.bind("127.0.0.1:8080").unwrap()
+        server.bind(get_address_port()).unwrap()
     };
 
     server.run()
---
+++ start-dev.sample.sh
@@ -0,0 +1,2 @@
+export ADDRESS="0.0.0.0"
+export PORT="80"
