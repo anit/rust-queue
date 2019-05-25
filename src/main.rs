bc9f8fdbcc98fc2898546dc425cfbc112c2b1183
anit <anit.rai@cyphertree.com>
2019-05-25 16:47:10 +0530

    auto reload server

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 extern crate actix_web;
+extern crate listenfd;
+
+use listenfd::ListenFd;
 use actix_web::{web, HttpServer, App, HttpRequest};
 
-fn index (req: HttpRequest) -> &'static str {
+fn index (_req: HttpRequest) -> &'static str {
     "Hello World!"
 }
 
 
 fn main() -> std::io::Result<()> {
-    HttpServer::new(
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(
         || App::new().service(
             web::resource("/").to(index))
-        )
-        .bind("127.0.0.1:8080")
-        .expect("Failed to start the server")
-        .run()
+        );
+
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l).unwrap()
+    } else {
+        server.bind("127.0.0.1:8080").unwrap()
+    };
+
+    server.run()
 }
