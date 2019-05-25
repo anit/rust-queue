ad3ac7706ca306efb2a897be815d62ec3f0753fe
anit <anit.rai@cyphertree.com>
2019-05-25 16:16:10 +0530

    add actix web hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+extern crate actix_web;
+use actix_web::{web, HttpServer, App, HttpRequest};
+
+fn index (req: HttpRequest) -> &'static str {
+    "Hello World!"
+}
+
+
+fn main() -> std::io::Result<()> {
+    HttpServer::new(
+        || App::new().service(
+            web::resource("/").to(index))
+        )
+        .bind("127.0.0.1:8080")
+        .expect("Failed to start the server")
+        .run()
 }
